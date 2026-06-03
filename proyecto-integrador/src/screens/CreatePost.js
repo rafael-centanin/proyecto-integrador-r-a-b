5aa6ec0d676eb2f9ec49b6ebc4bb7c91a377b97b
abrilfari <afarinaccia@udesa.edu.ar>
2026-06-02 23:31:13 -0300

    Agregados algunos estilos de botones y postep

--- proyecto-integrador/src/screens/CreatePost.js
+++ proyecto-integrador/src/screens/CreatePost.js
@@ -1,4 +1,4 @@
-import { View, Text, TextInput } from "react-native-web";
+import { View, Text, TextInput, StyleSheet } from "react-native-web";
 import { db, auth } from "../firebase/config"
 import { useState } from "react";
 import { Pressable } from "react-native";
@@ -12,7 +12,7 @@ function CreatePost() {
             owner: auth.currentUser.email,
             descripcionPost: descripcion,
             createdAt: Date.now(),
-            likes:[]
+            likes: []
         })
             .then()
             .catch(e => console.log(e))
@@ -20,14 +20,49 @@ function CreatePost() {
     }
 
     return (
-        <View>
-            <Text> Crear post</Text>
-            <TextInput placeholder="Escribí tu post" value={descripcion} onChangeText={(text)=> setDescripcion(text)}/>
-            <Pressable onPress={onSubmit}>
-                <Text>Publicar</Text>
+        <View style={styles.container}>
+            <Text style={styles.h1}> Crear nuevo post</Text>
+            <TextInput style={styles.Input} placeholder="Escribí tu posteo..." value={descripcion} onChangeText={(text) => setDescripcion(text)} />
+            <Pressable style={styles.Boton} onPress={onSubmit}>
+                <Text style={styles.BotonText}>Publicar Post</Text>
             </Pressable>
         </View>
     )
 }
 
 export default CreatePost;
+
+const styles = StyleSheet.create({
+    h1: {
+        fontSize: 32,
+        fontWeight: 'bold',
+        color: '#000000',
+        marginVertical: 10,
+    },
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        marginTop:40
+    },
+    Input: {
+        height: 20,
+        paddingVertical: 50,
+        paddingHorizontal: 50,
+        borderWidth: 1,
+        borderStyle: "solid",
+        marginVertical: 10,
+    },
+    Boton: {
+        backgroundColor: "#28a3a7",
+        paddingHorizontal: 10,
+        paddingVertical: 6,
+        textAlign: "center",
+        borderRadius: 4,
+        borderWidth: 1,
+        borderStyle: "solid",
+        borderColor:"#28a3a7",
+    },
+    BotonText: {
+        color: "#fff",
+    }
+},)
--- proyecto-integrador/src/screens/Profile.js
+++ proyecto-integrador/src/screens/Profile.js
@@ -1,21 +1,21 @@
 import React from "react"
-import {View, Text, Pressable, StyleSheet} from 'react-native';
+import { View, Text, Pressable, StyleSheet } from 'react-native';
 import { auth } from "../firebase/config";
 
 function Profile(props) {
-    function Logout() {
-        props.navigation.navigate('Login')
-        auth.signOut()
-    }
-    return(
-        <View style={styles.container}>
-            <Text style={styles.title}>Profile</Text>
-             <Pressable
-             onPress={ ()=> Logout()}>
-                <Text style={styles.texto}> Desloguearse </Text>
-            </Pressable>
-        </View>
-    )
+  function Logout() {
+    props.navigation.navigate('Login')
+    auth.signOut()
+  }
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Profile</Text>
+      <Pressable style={styles.Boton}
+        onPress={() => Logout()}>
+        <Text style={styles.texto}> Desloguearse </Text>
+      </Pressable>
+    </View>
+  )
 }
 export default Profile;
 const styles = StyleSheet.create({
@@ -25,9 +25,23 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   title: {
-    fontSize: 25,
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: '#000000',
+    marginVertical: 10,
+  },
+  Boton: {
+    backgroundColor: "#28a3a7",
+    paddingHorizontal: 10,
+    paddingVertical: 6,
+    textAlign: "center",
+    borderRadius: 4,
+    borderWidth: 1,
+    borderStyle: "solid",
+    borderColor: "#28a3a7",
   },
   texto: {
     fontSize: 18,
+    color: "#fff",
   },
 });
