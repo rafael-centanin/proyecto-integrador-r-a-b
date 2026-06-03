55d15a007fcc506df47d85c0ca7af41f6552885d
rafael-centanin <rcentaninmoline@udesa.edu.ar>
2026-06-03 17:47:59 -0300

    Avance con el profile.js

--- proyecto-integrador/src/screens/Profile.js
+++ proyecto-integrador/src/screens/Profile.js
@@ -1,23 +1,66 @@
-import React from "react"
+import React, { useEffect, useState } from "react";
 import { View, Text, Pressable, StyleSheet } from 'react-native';
-import { auth } from "../firebase/config";
+import { auth, db } from "../firebase/config";
 
 function Profile(props) {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
   function Logout() {
-    props.navigation.navigate('Login')
-    auth.signOut()
+    auth.signOut();
+    props.navigation.navigate('Login');
   }
+
+  useEffect(() => {
+    auth.onAuthStateChanged(user => {
+      if (user == "") {
+        return;
+      }
+      db.collection('users').where('owner', '==', auth.currentUser.email).onSnapshot(docs => {
+        let user = [];
+        docs.forEach(doc => {
+          user.push({
+            id: doc.id,
+            data: doc.data()
+          });
+        });
+        setUser(user);
+    console.log(user)
+
+        setLoading(false);
+      });
+    });
+
+  }, [])
+
+
+
+  //  db.collection('users').where('owner', '==', auth.currentUser.user).onSnapshot(docs => {
+  //     let user = [];
+  //     docs.forEach(doc => {
+  //       posts.push({
+  //         id: doc.id,
+  //         data: doc.data()
+  //       });
+  //     });
+  //     setUser(user); 
+  //     setLoading(false);
+  //   });
+  // }, []);
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Profile</Text>
-      <Pressable style={styles.Boton}
-        onPress={() => Logout()}>
-        <Text style={styles.texto}> Desloguearse </Text>
+      {loading === true ? <Text>cargando...</Text> : <Text> {user[0].data.User}</Text>}
+      <Pressable style={styles.Boton} onPress={() => Logout()}>
+        <Text style={styles.texto}>Desloguearse</Text>
       </Pressable>
     </View>
-  )
+  );
 }
+
 export default Profile;
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
