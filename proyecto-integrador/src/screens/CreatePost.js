03cbbb8460b594c2c75e2692aa27e0bc9c53c96c
abrilfari <afarinaccia@udesa.edu.ar>
2026-06-02 22:59:24 -0300

    Parte del crear posteo hecho

--- proyecto-integrador/src/screens/CreatePost.js
+++ proyecto-integrador/src/screens/CreatePost.js
@@ -1,6 +1,24 @@
 import { View, Text } from "react-native-web";
-function CreatePost(){
-    return(
+import { db, auth } from "../firebase/config"
+import { useState } from "react";
+
+function CreatePost() {
+
+    const [descripcion, setDescripcion] = useState("");
+
+    function onSubmit() {
+        db.collection("posts").add({
+            owner: auth.currentUser.email,
+            descripcionPost: descripcion,
+            createdAt: Date.now(),
+            likes:[]
+        })
+            .then()
+            .catch(e => console.log(e))
+
+    }
+
+    return (
         <View>
             <Text> Crear post</Text>
         </View>
