e2164673805e7646e03fee42216f19199ca2ab78
btorresdiezgomez <btorresdiezgomez@udesa.edu.ar>
2026-06-15 20:18:48 -0300

    Agruegue que se vea el post en el comment y que se vea el user en el comentario

--- proyecto-integrador/src/screens/Comments.js
+++ proyecto-integrador/src/screens/Comments.js
@@ -9,10 +9,16 @@ function Comments(props) {
     const postId = props.route.params.id
     const [coment, setComent] = useState("")
     const [ArrayComentario, setArrayComentario] = useState([])
+    const [Posts, setPosts] = useState("")
+    const likes = Posts.likes || [] //Agrego esto porque sino se nos rompe todo cuando no hay likes lol -Benja
 
     useEffect(() => {
         const comentario = db.collection('posts').doc(postId).onSnapshot(doc => {
             const comentarios = doc.data().comentario
+            const PostActual = doc.data()
+            console.log(PostActual)
+            setPosts(PostActual)
+
             if (comentarios) {
                 setArrayComentario(comentarios)
             }
@@ -21,10 +27,14 @@ function Comments(props) {
     }, [postId])
 
     function actualizarDatos() {
+        let ComentarioUsuarios = {
+            texto: coment,
+            owner: auth.currentUser.email //Tenemos que hacer un objeto literal para que aparezca ahora el user que escribio
+        }
         db.collection('posts')
             .doc(postId)
             .update({
-                comentario: firebase.firestore.FieldValue.arrayUnion(coment)
+                comentario: firebase.firestore.FieldValue.arrayUnion(ComentarioUsuarios)
             })
             .then(() => {
                 setComent("")
@@ -36,30 +46,43 @@ function Comments(props) {
 
     return (
         <View style={styles.container}>
-            <Text style={styles.h1}> Comentarios</Text>
+            <View>
+                <Text>{Posts.owner}</Text>
+                <Text>{Posts.descripcionPost}</Text>
+                <Text>{likes.length > 0 ? likes.length : "No hay "} Likes</Text>
+            </View>
+            <Text style={styles.h2}> Comentarios</Text>
             <TextInput style={styles.Input} placeholder="Comenta!" value={coment} onChangeText={(text) => setComent(text)} />
             <Pressable style={styles.Boton} onPress={() => actualizarDatos()}>
                 <Text style={styles.BotonText}>Comentar</Text>
             </Pressable>
-            <Text style={styles.h3}>Tus comentarios anteriores</Text>
+            <Text style={styles.h3}>  Comentarios anteriores</Text>
             <FlatList
                 data={ArrayComentario}
                 keyExtractor={item => item}
                 renderItem={({ item }) => (<View style={styles.containerAnt}>
-                    <Text style={styles.text}>{item}</Text>
+                    <Text style={styles.text}>{item.owner}</Text>
+                    <Text style={styles.text}>{item.texto}</Text>
                 </View>)}
             />
         </View>
     )
 }
 export default Comments;
-//<Text style={styles.text}>{item}</Text>
 
 const styles = StyleSheet.create({
     h1: {
         textAlign: "center",
         fontFamily: 'Courier',
         fontSize: 55,
+        fontWeight: 'bold',
+        color: '#64090E',
+        marginVertical: 10,
+    },
+    h2: {
+        textAlign: "center",
+        fontFamily: 'Courier',
+        fontSize: 40,
         fontWeight: 'bold',
         color: '#64090E',
         marginVertical: 10,
@@ -78,7 +101,7 @@ const styles = StyleSheet.create({
         width: "100%",
         flex: 1,
         alignItems: 'center',
-        paddingTop: 50,  
+        paddingTop: 50,
     },
     containerAnt: {
         backgroundColor: '#64090E',
